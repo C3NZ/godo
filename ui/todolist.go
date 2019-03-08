934537069af2befaa79bd326519171500e420eff
C3NZ <cenz@cenz.io>
2019-03-08 02:58:23 -0800

    [fix] format code

--- ui/todolist.go
+++ ui/todolist.go
@@ -28,13 +28,13 @@ func CreateTodoWindow() {
 	createTodoEdit(todoWindow)
 }
 
-func updateTasks () {
-    tasks, err := db.AllTasks()
+func updateTasks() {
+	tasks, err := db.AllTasks()
 	if err != nil {
 		panic(err)
 	}
 	for id, task := range tasks {
-	    taskMap[id + 1] = task.Key
+		taskMap[id+1] = task.Key
 		num := strconv.Itoa(id + 1)
 		todoList.AddItem(num + "." + task.Value)
 	}
@@ -46,18 +46,19 @@ func createTodoList(view *clui.Window) {
 	todoList.OnSelectItem(func(e clui.Event) {
 		item := todoList.FindItem(e.Msg, true)
 		if item != -1 {
-            strID := string(strings.Split(e.Msg, " ")[0][0])
-            intID, err := strconv.Atoi(strID); if err != nil {
-                panic(err)
-            }
+			strID := string(strings.Split(e.Msg, " ")[0][0])
+			intID, err := strconv.Atoi(strID)
+			if err != nil {
+				panic(err)
+			}
 			todoList.RemoveItem(item)
-            todoList.Clear()
-		    DeleteItem(taskMap[intID])
+			todoList.Clear()
+			DeleteItem(taskMap[intID])
 			updateTasks()
-		    
-		} 
+
+		}
 	})
-    updateTasks()
+	updateTasks()
 }
 
 // CreateTodoEdit creates the Edit field for the adding todos
@@ -73,8 +74,8 @@ func createTodoEdit(view *clui.Window) {
 	todoEdit.OnKeyPress(func(key term.Key, testRune rune) bool {
 		if key == term.KeyEnter {
 			currentTask = strings.Split(currentTask, ":")[1]
-            AddItem(currentTask)
-            todoList.Clear()
+			AddItem(currentTask)
+			todoList.Clear()
 			updateTasks()
 			todoEdit.Clear()
 			todoEdit.SetTitle(taskMsg)
