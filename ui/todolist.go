d8e22ba43edaf16d453ff2a79d78f1d71fbdbf3f
C3NZ <cenz@cenz.io>
2019-04-17 08:23:54 -0700

    [fix] refactor handler functions to be their own functions instead of anonymous ones

--- ui/todolist.go
+++ ui/todolist.go
@@ -11,6 +11,7 @@ import (
 )
 
 var todoList *clui.ListBox
+var todoEdit *clui.EditField
 var currentTask string
 var taskMsg = "Enter task:"
 var taskMap = make(map[int]int)
@@ -28,11 +29,17 @@ func CreateTodoWindow() {
 	createTodoEdit(todoWindow)
 }
 
+// updateTasks will update all the tasks to be displayed on the
+// to do list
 func updateTasks() {
+
+	// Grab all tasks from the db
 	tasks, err := db.AllTasks()
 	if err != nil {
 		panic(err)
 	}
+
+	// iterate over all of the retrieved tasks
 	for id, task := range tasks {
 		taskMap[id+1] = task.Key
 		num := strconv.Itoa(id + 1)
@@ -40,50 +47,59 @@ func updateTasks() {
 	}
 }
 
+// Handler function for when an item is being selected
+func handleItemSelected(e clui.Event) {
+	// anonymous handler for the todolist
+
+	// Find the todo list item based on where we searched
+	if item := todoList.FindItem(e.Msg, true); item != -1 {
+		strID := string(strings.Split(e.Msg, " ")[0][0])
+		intID, err := strconv.Atoi(strID)
+		if err != nil {
+			panic(err)
+		}
+		todoList.RemoveItem(item)
+		todoList.Clear()
+		DeleteItem(taskMap[intID])
+		updateTasks()
+
+	}
+}
+
 // CreateTodoList creates the Listbox for displaying the todos
 func createTodoList(view *clui.Window) {
 	todoList = clui.CreateListBox(view, 30, 20, 50)
-	todoList.OnSelectItem(func(e clui.Event) {
-		item := todoList.FindItem(e.Msg, true)
-		if item != -1 {
-			strID := string(strings.Split(e.Msg, " ")[0][0])
-			intID, err := strconv.Atoi(strID)
-			if err != nil {
-				panic(err)
-			}
-			todoList.RemoveItem(item)
-			todoList.Clear()
-			DeleteItem(taskMap[intID])
-			updateTasks()
-
-		}
-	})
+	todoList.OnSelectItem(handleItemSelected)
 	updateTasks()
+}
+
+// Handle when a key is pressed within the edit field
+func handleKeyPress(key term.Key, testRune rune) bool {
+	if key == term.KeyEnter {
+		currentTask = strings.Split(currentTask, ":")[1]
+		AddItem(currentTask)
+		todoList.Clear()
+		updateTasks()
+		todoEdit.Clear()
+		todoEdit.SetTitle(taskMsg)
+		return true
+	}
+
+	return false
 }
 
 // CreateTodoEdit creates the Edit field for the adding todos
 func createTodoEdit(view *clui.Window) {
-	todoEdit := clui.CreateEditField(view, 30, taskMsg, 50)
+	todoEdit = clui.CreateEditField(view, 30, taskMsg, 50)
 
-	// Register the todo edit on change
+	// Register the todo edit on key press
+	todoEdit.OnKeyPress(handleKeyPress)
+
+	// Register the todo edit on change (update the current task)
 	todoEdit.OnChange(func(e clui.Event) {
 		currentTask = e.Msg
 	})
 
-	// Register the todo edit on key press
-	todoEdit.OnKeyPress(func(key term.Key, testRune rune) bool {
-		if key == term.KeyEnter {
-			currentTask = strings.Split(currentTask, ":")[1]
-			AddItem(currentTask)
-			todoList.Clear()
-			updateTasks()
-			todoEdit.Clear()
-			todoEdit.SetTitle(taskMsg)
-			return true
-		}
-
-		return false
-	})
 }
 
 // AddItem adds an item to the task list
